94dbc6774fe9a34b2e02d7194b4270cad6bcb060
Ingmar Kole <ikole@student.codam.nl>
2021-07-30 16:47:27 +0200

    ops: iterator class structure

--- iterator/iterator.hpp
+++ iterator/iterator.hpp
@@ -22,48 +22,164 @@
 
 namespace ft {
 
-	# define tpl template <class Category, class T, class Distance = std::ptrdiff_t, class Pointer = T*, class Reference = T&>
+/*
+** Tags
+*/
+	struct input_iterator_tag			{};
+	struct output_iterator_tag			{};
+	struct forward_iterator_tag			{};
+	struct bidirectional_iterator_tag	{};
+	struct random_access_iterator_tag	{};
+
+/*
+** Iterator base class
+*/
+	# define tpl template <class Category, class T, class Distance = std::ptrdiff_t, class Pointer = T*, class Reference = T&>	
 	
 	tpl
-	struct iterator {
-		typedef T         value_type;
-		typedef Distance  difference_type;
-		typedef Pointer   pointer;
-		typedef Reference reference;
-		typedef Category  iterator_category;
-		
-		iterator();
-		iterator(const iterator& x);
-		iterator&	operator  = (const iterator& x);
-		bool		operator == (const iterator& x);
-		bool		operator != (const iterator& x);
-		iterator*	operator -> ();
-		iterator	operator -> ();
-		iterator&	operator ++ ();		//prefix
-		iterator	operator ++ (int);	//postfix
-		iterator&	operator -- ();		//prefix
-		iterator	operator -- (int);	//postfix
-		iterator	operator  + (int);
-		iterator	operator  + (const iterator& x);
-		iterator	operator += (int);
-		iterator	operator += (const iterator& x);
-		iterator	operator  - (int);
-		iterator	operator  - (const iterator& x);
-		iterator	operator -= (int);
-		iterator	operator -= (const iterator& x);
-		iterator	operator [] (std::size_t n);
-		bool		operator  < (const iterator& x);
-		bool		operator <= (const iterator& x);
-		bool		operator  > (const iterator& x);
-		bool		operator >= (const iterator& x);
+	struct iterator
+	{
+	/*
+	** Typedefs 
+	*/
+		typedef T			value_type;
+		typedef Distance	difference_type;
+		typedef Pointer		pointer;
+		typedef Reference	reference;
+		typedef Category	iterator_category;
+
+	/*
+	** Common operators
+	*/
+	protected:
+		reference	operator ++ ();		//prefix
+		value_type	operator ++ (int);	//postfix
+		reference	operator  = (const iterator& x);
+
+	/*
+	** Member variables
+	*/
+		pointer				_ptr;
+		iterator_category	_cat;
 	};
 
+/*
+** InputIterator
+*/
 	tpl
-	class RandomAccessIterator : public iterator {
+	class InputIterator : public iterator
+	{
+	/*
+	** Constructor
+	*/
+	private:
+		InputIterator();
 
+	public:
+		InputIterator(const InputIterator& x);
+		virtual	~InputIterator();
+
+	/*
+	** Operator overload
+	*/
+	protected:
+		bool			operator == (const InputIterator& x);
+		bool			operator != (const InputIterator& x);
+		Pointer			operator -> ();
+		value_type	operator -> ();
 	};
 
-	#undef tpl
+/*
+**	OutputIterator
+*/
+	tpl
+	class OutputIterator : public iterator
+	{
+	/*
+	** Constructor
+	*/
+	private:
+		OutputIterator();
+	
+	public:
+		OutputIterator(const OutputIterator& x);
+		virtual	~OutputIterator();
+
+	/*
+	** Operator overload
+	*/
+		reference	operator -> ();
+	};
+
+/*
+** Forward iterator
+*/
+	tpl
+	class ForwardIterator : public virtual InputIterator, public virtual OutputIterator
+	{
+	/*
+	** Constructor
+	*/
+	public:
+		ForwardIterator();
+		ForwardIterator(const ForwardIterator& x);
+		virtual ~ForwardIterator();
+	};
+
+/*
+** Bidirectional iterator
+*/
+	tpl
+	class BidirectionalIterator : public ForwardIterator
+	{
+	/*
+	** Constructor
+	*/
+	public:
+		ForwardIterator();
+		ForwardIterator(const ForwardIterator& x);
+		virtual ~ForwardIterator();
+	
+	/*
+	** Operator overload
+	*/
+	protected:
+		reference	operator -- ();		//prefix
+		value_type	operator -- (int);	//postfix
+	};
+
+/*
+** Random access iterator
+*/
+	tpl
+	class RandomAccessIterator : public BidirectionalIterator
+	{
+	/*
+	** Constructor
+	*/
+	public:
+		ForwardIterator();
+		ForwardIterator(const ForwardIterator& x);
+		virtual ~ForwardIterator();
+	
+	/*
+	** Operator overload
+	*/
+	protected:
+		value_type	operator  + (int);
+		value_type	operator  + (const Iterator& x);
+		value_type	operator  - (int);
+		value_type	operator  - (const Iterator& x);
+		bool		operator  < (const Iterator& x);
+		bool		operator  > (const Iterator& x);
+		bool		operator <= (const Iterator& x);
+		bool		operator >= (const Iterator& x);
+		value_type	operator += (int);
+		value_type	operator -= (int);
+		value_type	operator [] (std::size_t n);
+	};
+
+	# undef tpl
 }
 
 #endif
