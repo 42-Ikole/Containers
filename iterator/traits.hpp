31aa843f4123e1a01b5d7845669718cb4712c1d7
Ingmar Kole <ikole@student.codam.nl>
2021-08-08 14:18:13 +0200

    feat: added iterator_trait specialisation for pointer and const pointer

--- iterator/traits.hpp
+++ iterator/traits.hpp
@@ -20,19 +20,6 @@
 
 namespace ft
 {
-////////////////////////////
-// true type / false type //
-////////////////////////////
-	template <class T, T v>
-		struct integral_constant {
-			static const T value = v;
-		};
-
-	template <bool B>
-		struct bool_constant : integral_constant<bool, B>{};
-
-		struct true_type : bool_constant<true>{};
-		struct false_type : bool_constant<false>{};
 
 //////////
 // Tags //
@@ -41,64 +28,6 @@ namespace ft
 	struct forward_iterator_tag : input_iterator_tag				{};
 	struct bidirectional_iterator_tag : forward_iterator_tag		{};
 	struct random_access_iterator_tag : bidirectional_iterator_tag	{};
-
-
-	// ////////////////////
-	// // Input iterator //
-	// ////////////////////
-
-	// template< class T >
-	// struct is_input_iterator {
-	// 	static const bool value = false;
-	// };
-
-	// template<>
-	// struct is_input_iterator<input_iterator_tag> {
-	// 	static const bool value = true;
-	// };
-
-	// //////////////////////
-	// // forward iterator //
-	// //////////////////////
-
-	// template< class T >
-	// struct is_forward_iterator {
-	// 	static const bool value = false;
-	// };
-
-	// template<>
-	// struct is_forward_iterator<forward_iterator_tag> {
-	// 	static const bool value = true;
-	// };
-
-	// ////////////////////////////
-	// // bidirectional iterator //
-	// ////////////////////////////
-
-	// template< class T >
-	// struct is_bidirectional_iterator {
-	// 	static const bool value = false;
-	// };
-
-	// template<>
-	// struct is_bidirectional_iterator<bidirectional_iterator_tag> {
-	// 	static const bool value = true;
-	// };
-
-	// ////////////////////////////
-	// // random access iterator //
-	// ////////////////////////////
-
-	// template< class T >
-	// struct is_random_access_iterator {
-	// 	static const bool value = false;
-	// };
-
-	// template<>
-	// struct is_random_access_iterator<random_access_iterator_tag> {
-	// 	static const bool value = true;
-	// };
-
 
 /////////////////////
 // Iterator traits //
@@ -113,51 +42,27 @@ namespace ft
 		typedef typename  Iter::iterator_category	iterator_category;
 	};
 
+	/* pointer specialisation */
 	template< class T >
-		struct iterator_traits<T*>;
-
-	template< class T >
-		struct iterator_traits<const T*>;
-
-///////////////////////////
-// has iterator category //
-///////////////////////////
-
-	/* what the actual fuck is this magic spell?!?! */
-	template <class Tp>
-		struct has_iterator_category
+		struct iterator_traits<T*>
 	{
-	private:
-		struct two {char lx; char lxx;};
-		template <class Up> static two test(...);
-		template <class Up> static char test(typename Up::iterator_category* = 0);
-	public:
-		static const bool value = sizeof(test<Tp>(0)) == 1;
+		typedef T							value_type;
+		typedef std::ptrdiff_t				difference_type;
+		typedef T*							pointer;
+		typedef T&							reference;
+		typedef random_access_iterator_tag	iterator_category;
 	};
 
-	template <class Tp, class Up, bool = has_iterator_category<iterator_traits<Tp> >::value>
-		struct has_iterator_category_convertible_to
-			: public std::integral_constant<bool, std::is_convertible<typename iterator_traits<Tp>::iterator_category, Up>::value>
-	{};
-
-	template <class Tp, class Up>
-		struct has_iterator_category_convertible_to<Tp, Up, false> : public false_type {};
-
-/////////////////
-// is iterator //
-/////////////////
-
-	template <class Tp>
-		struct is_input_iterator : public has_iterator_category_convertible_to<Tp, input_iterator_tag> {};
-
-	template <class Tp>
-		struct is_forward_iterator : public has_iterator_category_convertible_to<Tp, forward_iterator_tag> {};
-
-	template <class Tp>
-		struct is_bidirectional_iterator : public has_iterator_category_convertible_to<Tp, bidirectional_iterator_tag> {};
-
-	template <class Tp>
-		struct is_random_access_iterator : public has_iterator_category_convertible_to<Tp, random_access_iterator_tag> {};
+	/* const pointer specialisation */
+	template< class T >
+		struct iterator_traits<const T*>
+	{
+		typedef T							value_type;
+		typedef std::ptrdiff_t				difference_type;
+		typedef const T*					pointer;
+		typedef const T&					reference;
+		typedef random_access_iterator_tag	iterator_category;
+	};
 
 }
 
