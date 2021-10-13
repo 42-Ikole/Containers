99ac0ddd4a8463dcb174e3dd96ad4d30d904c518
Ingmar Kole <ikole@student.codam.nl>
2021-10-13 18:01:54 +0200

    Final implementation of deque as a circular buffer

--- deque/deque.hpp
+++ deque/deque.hpp
@@ -18,7 +18,7 @@
 # include <memory>
 # include <array_iterator.hpp> //
 # include <general_helpers.hpp>
-# include <linke_list.hpp>
+# include <circular_buffer.hpp>
 
 namespace ft
 {
@@ -33,232 +33,153 @@ namespace ft
 	///////////////
 	// type defs //
 	///////////////
-		public:
+	public:
 
-			typedef T																							value_type;
-			typedef Alloc																						allocator_type;
-			typedef std::size_t																					size_type;
-			typedef std::ptrdiff_t																				difference_type;
-			typedef value_type&																					reference;
-			typedef const value_type&																			const_reference;
-			typedef value_type*																					pointer;
-			typedef const value_type*																			const_pointer;
-			typedef linke_list<T>																				node;
-			typedef node*																						node_pointer;
-			typedef std::allocator<node>																		node_allocator_type;
+		typedef T																							value_type;
+		typedef Alloc																						allocator_type;
+		typedef std::size_t																					size_type;
+		typedef std::ptrdiff_t																				difference_type;
+		typedef value_type&																					reference;
+		typedef const value_type&																			const_reference;
+		typedef value_type*																					pointer;
+		typedef const value_type*																			const_pointer;
+		typedef circular_buffer<T>																			cbuf;
+		typedef	std::allocator<cbuf>																		cbuf_allocator_type;
 
-			/* GA EEN NODE ITERATOR SCHRIJVEN */
-			typedef ft::array_iterator<T, ft::random_access_iterator_tag>										iterator;
-			typedef ft::array_iterator<T, ft::random_access_iterator_tag, std::ptrdiff_t, const T*, const T&>	const_iterator;
-			typedef ft::reverse_iterator< iterator >															reverse_iterator;
-			typedef ft::reverse_iterator< const_iterator >														const_reverse_iterator;
+		/* GA EEN NODE ITERATOR SCHRIJVEN */
+		typedef ft::array_iterator<T, ft::random_access_iterator_tag>										iterator;
+		typedef ft::array_iterator<T, ft::random_access_iterator_tag, std::ptrdiff_t, const T*, const T&>	const_iterator;
+		typedef ft::reverse_iterator< iterator >															reverse_iterator;
+		typedef ft::reverse_iterator< const_iterator >														const_reverse_iterator;
 
 	//////////////////////
 	// Member variables //
 	//////////////////////
-		private:
+	private:
 
-			allocator_type		_alloc;
-			node_pointer		_head;
-			node_pointer		_tail;
-			size_type			_size;
-			node_allocator_type	_node_alloc;
+		cbuf_allocator_type	_alloc;
+		cbuf				*_arr; /* dubbele pointer om niet de copy constructor te hoeven callen */
+		size_type			_head;
+		size_type			_tail;
+		size_type			_capacity;
+		size_type			_size;
 
 	/////////////////////
 	// BOB THE BUILDER //
 	/////////////////////
 	public:
 
-		explicit deque(const allocator_type& alloc = allocator_type())
-			: _alloc(alloc), _head(_allocate_node()), _tail(_head), _size(0), _node_alloc(node_allocator_type())
+		explicit deque (const allocator_type& alloc = allocator_type())
+			: _alloc(cbuf_allocator_type()), _arr(NULL), _capacity(128), _size(0)
 		{
-		}
-
-		explicit deque(size_type n, const T& val = T(), const allocator_type& alloc = allocator_type())
-			: _alloc(alloc), _head(_allocate_node()), _tail(_head), _size(0), _node_alloc(node_allocator_type())
-		{
-			_tail = _tail->add_range_back(n, val);
-		}
-
-		template< class InputIt >
-			deque(InputIt first, InputIt last,	const allocator_type& alloc = allocator_type())
-				: _alloc(alloc), _head(_allocate_node()), _tail(_head), _size(0), _node_alloc(node_allocator_type())
-		{
-			_tail = _head->add_range_back(first, last);
-		}
-		
-		deque(const deque& x)
-		{
-			*this = x;
-		}
-
-		deque&	operator = (const deque &x)
-		{
-			this->clear();
-			this->_head = x._head;
-			this->_tail = x._tail;
-			this->_size = x._size;
-			return (*this);
+			_arr = _alloc.allocate(_capacity);
+			_head = 0;
+			_tail = 0;
+			this->_construct_element(_head);
 		}
 
 	//////////////////////////////
-	// Private member functions //
+	// private member functions //
 	//////////////////////////////
-		private:
+	private:
 
-			void			_destruction()
-			{
-				while(_size > 0)
-					this->pop_back();
+		void	_realloc()
+		{
+			cbuf		*tmp;
+
+			_capacity <<= 1;
+			tmp = _alloc.allocate(_capacity);
+			size_type i = 0;
+			for (; _head != _tail; i++) {
+				if (_head == _capacity >> 1)
+					_head = 0;
+				_alloc.construct(&tmp[i], _arr[_head]);
+				_alloc.destroy(_arr[_head]);
+				_head++;
 			}
+			_tail = i;
+			_head = 0;
+			_alloc.deallocate(_arr);
+			_arr = tmp;
+		}
 
-			node_pointer	_allocate_node(const node& new_node = node())
-			{
-				node_pointer ret;
+		void	_construct_element(size_type& idx, cbuf& = cbuf())
+		{
+			_alloc.construct(&_arr[idx], val);
+		}
 
-				ret = _node_alloc.allocate(1);
-				_node_alloc.construct(ret, new_node);
-				return (ret);
-			}
+		void	_destroy_element(size_type& idx)
+		{
+			_alloc.destroy(&_arr[idx]);
+		}
 
-			reference		_get_element(size_type n)
-			{
-				node_pointer tmp = _head;
-
-				while (n > tmp->size())
-				{
-					n -= tmp->size();
-					tmp = tmp->_next;
-				}
-				return ((*tmp)[n]);
-			}
-
-	//////////////
-	// CAPACITY //
-	//////////////
-		public:
-
-			size_type	size() const
-			{
-				return (_size);
-			}
-
-			size_type	max_size() const
-			{
-				return (_alloc.max_size());
-			}
-
-			void		resize(size_type n, value_type val = value_type())
-			{
-				if (n <= _size) {
-					for (size_type i = _size; i > n; i--)
-						this->pop_back();
-				}
-				else {
-					for (size_type i = _size; i < n; i++)
-						this->push_back(val);
-				}
-			}
-
-			bool		empty() const
-			{
-				return (_size == 0);
-			}
+		bool	_empty()
+		{
+			return (_size == 0);
+		}
 
 	///////////////
 	// MODIFIERS //
 	///////////////
 	public:
 
-		/* iterator range assign */
-		template	<class InputIterator>
-			void		assign(InputIterator first, InputIterator last,
-				typename ft::iterator_traits<InputIterator>::iterator_category* = 0)
+		/* assign */
+
+		void push_back(const value_type& val)
+		{
+			if (_arr[_tail].is_full()) {
+				_tail++;
+				if (_tail == _capacity)
+					_tail = 0;
+				// if (this->is_full())
+				// 	realloc ofzo
+				this->_construct_element(_tail);
+			}
+			_arr[_tail].emplace_back(val);
+			_size++;
+		}
+
+		void push_front(const value_type& val)
+		{
+			if (_arr[_head].is_full()) {
+				_head--;
+				if (_head == -1)
+					_head = _capacity - 1;
+				// if (this->is_full())
+				// 	realloc ofzo
+				this->_construct_element(_head);
+			}
+			_arr[_head].emplace_front(val);
+			_size++;
+		}
+
+		void pop_back()
+		{
+			if (this->_empty())
+				return ;
+			_arr[_tail].dequeue_back();
+			if (_arr[_tail].empty())
 			{
-				this->clear();
-				while (first != last)
-				{
-					this->push_back(*first);
-					first++;
+				if (_tail != _head) {
+					this->_destroy_element(_tail);
+					_tail--;
 				}
 			}
+		}
 
-			/* fill assign */
-			void		assign(size_type n, const value_type& val)
+		void pop_front()
+		{
+			if (this->_empty())
+				return ;
+			_arr[_head].dequeue_front();
+			if (_arr[_head].empty())
 			{
-				this->clear();
-				for (; n > 0; n--)
-					this->push_back(val);
+				if (_head != _tail) {
+					this->_destroy_element(_head);
+					_head++;
+				}
 			}
-
-			void		push_back(value_type& val)
-			{
-				_tail = _tail->push_back(val);
-				_size++;
-			}
-
-			void		pop_back()
-			{
-				if (_size <= 0)
-					return ;
-				_tail = _tail->pop_back();
-				_size--;
-			}
-
-			void		push_front(value_type& val)
-			{
-				_head = _head->push_front(val);
-				_size++;
-			}
-
-			void		pop_front()
-			{
-				if (_size <= 0)
-					return ;
-				_size--;
-				_head = _head->pop_front();
-			}
-
-			/*
-			** INSERT MAKES NO SENSE IN DEQUE!
-			*/
-
-			/*
-			** ERASE ALSO MAKES NO SENSE AT ALL :/
-			*/
-
-
-		////////////////////
-		// ELEMENT ACCESS //
-		////////////////////
-		public:
-
-			reference		operator[](size_type n)
-			{
-				return (this->_get_element(n));
-			}
-
-			const_reference	operator[](size_type n) const
-			{
-				return (this->_get_element(n));
-			}
-
-
-
-			void		swap(deque& x)
-			{
-				ft::value_swap(this->_head, x._head);
-				ft::value_swap(this->_tail, x._tail);
-				ft::value_swap(this->_size, x._size);
-			}
-
-			void		clear()
-			{
-				_destruction();
-				_head = NULL;
-				_tail = NULL;
-				_size = 0;
-			}
+		}
 
 	}; /* end of deque */
 
