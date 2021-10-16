55025e4378698eb2edc1bb08630ea9b5bf6e4469
Ingmar Kole <ikole@student.codam.nl>
2021-10-16 14:41:52 +0200

    Chore: ezpz

--- Makefile
+++ Makefile
@@ -71,7 +71,7 @@ run: re
 test: re
 	@clear
 	@printf "\n\033[33mcompiling ft::containters\n\n\033[0m"
-	@$(CC) $(TEST_FLAGS) $(DEBUG_FLAGS) $(TEST_SRC) -o $(FT)
+	@$(CC) $(TEST_FLAGS) $(TEST_SRC) -o $(FT)
 	@./$(FT) > $(FT).txt
 	@printf "\n\033[33mcompiling std::containers\n\n\033[0m"
 	@$(CC) $(TEST_FLAGS) $(TEST_SRC) -D STD -o $(STD)
--- tests/deque_test.cpp
+++ tests/deque_test.cpp
@@ -43,12 +43,12 @@ static void	modifier_test()
 	print_header("DEQUE MODIFIER");
 	ft::deque<int> deq;
 
-	// for(int i = 0; i < 42; i++)
-		deq.push_back(42);
-	// whats_in_this_container_yo(deq, "42");
-	// for (int i = 0; i < 42069; i++)
-	// 	deq.push_front(i);
-	// whats_in_this_container_yo(deq, "42069");
+	for(int i = 0; i < 2042; i++)
+		deq.push_back(i);
+	whats_in_this_container_yo(deq, "2042");
+	for (int i = 0; i < 42069; i++)
+		deq.push_back(i);
+	whats_in_this_container_yo(deq, "42069");
 }
 
 void		deque_test()
