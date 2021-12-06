52df3844e92a08453479dd88a02f8c94dd9fc7aa
Ingmar Kole <ikole@student.codam.nl>
2021-12-06 14:52:16 +0100

    Fix: appearantly im a moron

--- lexicographical_compare/lexicographical_compare.hpp
+++ lexicographical_compare/lexicographical_compare.hpp
@@ -21,13 +21,13 @@ namespace ft
 		bool lexicographical_compare(InputIt1 first1, InputIt1 last1,
 									 InputIt2 first2, InputIt2 last2)
 	{
-		for (; first1 != last1 && first2 != last2; ++first1, ++first2 ) {
+		for (; (first1 != last1) && (first2 != last2); ++first1, ++first2) {
 			if (*first1 < *first2)
-				return true;
-			else 
-				return false;
+				return (true);
+			if (*first2 < *first1)
+				return (false);
 		}
-		return (first1 == last1 && first2 != last2);
+		return (first1 == last1) && (first2 != last2);
 	}
 
 	template<class InputIt1, class InputIt2, class Compare>
@@ -35,13 +35,13 @@ namespace ft
 									 InputIt2 first2, InputIt2 last2,
 									 Compare comp)
 	{
-		for (; first1 != last1 && first2 != last2; ++first1, ++first2 ) {
+		for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
 			if (comp(*first1, *first2))
-				return true;
+				return (true);
 			if (comp(*first2, *first1))
-				return false;
+				return (false);
 		}
-		return (first1 == last1 && first2 != last2);
+		return (first1 == last1 && (first2 != last2));
 	}
 
 } /* end of namespace */
--- utility/utility.hpp
+++ utility/utility.hpp
@@ -39,7 +39,22 @@ namespace ft
 	template < class T1, class T2 >
 		ft::pair<T1, T2> make_pair (T1 x, T2 y)
 	{
-		return ( ft::pair<T1, T2>(x, y) );
+		return (ft::pair<T1, T2>(x, y));
+	}
+
+///////////
+// EQUAL //
+///////////
+	template< class InputIt1, class InputIt2 >
+		bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2)
+	{
+		while (first1 != last1) {
+			if (*first1 != *first2)
+				return (false);
+			++first1;
+			++first2;
+		}
+		return (true);
 	}
 
 } /* end of namespace */
