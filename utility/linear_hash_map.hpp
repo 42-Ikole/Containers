dbfe4a5c06c0d8b63bc927955e1d4f0bce526357
Ingmar Kole <ikole@student.codam.nl>
2021-12-13 18:44:58 +0100

    feat: starting on linear_hash map

---
+++ utility/linear_hash_map.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*      .'(     /`-.     )\.--.       .'(        )\   )\     /`-.      /`-.   */
+/*  ,') \  )  ,' _  \   (   ._.'  ,') \  )      (  ',/ /   ,' _  \   ,' _  \  */
+/* (  '-' (  (  '-' (    `-.`.   (  '-' (        )    (   (  '-' (  (  '-' (  */
+/*  ) .-.  )  )   _  )  ,_ (  \   ) .-.  )      (  \(\ \   )   _  )  ) ,._.'  */
+/* (  ,  ) \ (  ,' ) \ (  '.)  ) (  ,  ) \       `.) /  ) (  ,' ) \ (  '      */
+/*  )/    )/  )/    )/  '._,_.'   )/    )/           '.(   )/    )/  )/       */
+/*                                                                            */
+/*                   Kingmar  |  https://github.com/K1ngmar                   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LINEAR_HASH_MAP_ROBBIN_FROM_THE_HOOD_STYLE_HPP
+# define LINEAR_HASH_MAP_ROBBIN_FROM_THE_HOOD_STYLE_HPP                                                                          
+
+# include <hash.hpp>
+
+namespace ft
+{
+	template< class T, class Hash, class KeyEqual, class Allocator >
+		class linear_hash_map
+	{
+
+	}; /* end of linear_hash_map */
+
+} /* end of namespace */
+
+#endif
