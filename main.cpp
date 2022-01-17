6569165642732381b8f54d3cb6875fde12f3a99e
Ingmar Kole <ikole@student.codam.nl>
2022-01-17 13:47:15 +0100

    Chore: TeSsTiNg

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <type_traits.hpp>
 #include <is_integral.hpp>
 #include <type_traits>
+#include <linear_hash_map.hpp>
 
 struct test {
 	static const char* foek;
@@ -24,6 +25,24 @@ struct test {
 
 int main(void)
 {
+
+	typedef ft::linear_hash_map<int, ft::hash<int>, ft::equal_to<int> > hm_type;
+
+	hm_type				lhm;
+	hm_type::iterator	itr;
+
+	lhm.insert(42);
+	itr = lhm.insert(42).first;
+	lhm.insert(69);
+	lhm.insert(420);
+	lhm.insert(1337);
+
+	lhm.erase(itr);
+	for (hm_type::iterator i = lhm.begin(); i != lhm.end(); ++i)
+		std::cout << *i << std::endl;
+
+	
+
 	
 	// system("leaks containers | grep -o 'leaks for'");
 }
